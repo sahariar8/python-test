99fe6647c04c406a1392c096e4f3692810bcd154
sahariar8 <sahariaralam8@gmail.com>
2023-12-14 22:04:15 +0600

    python

---
+++ input.py
@@ -0,0 +1,26 @@
+'''
+name = input('Enter Your Name:');
+age = input('Enter Your age:');
+print('my name is '+name);
+print("i am ",age," years old");
+'''
+
+# age=int(input('Enter Your age :'));
+# print("my age is now ",age)
+# name=input("Enter your Name:");
+# print(name +" is my name");
+
+a=24;
+# b=7
+# print(a+b);
+# print(a-b);
+# print(a*b);
+# print(a/b);
+# print(a//b);
+# print(a%b);
+if a>20:
+    print("number is big")
+elif a>10:
+    print("number is middle")
+else:
+    print('number is samll')
